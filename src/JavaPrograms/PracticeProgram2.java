5d1ec66c4449b76d39ed3d50900cadf08fc5625c
Reshma <Reshma@LAPTOP-3PFHDHI7>
2024-06-14 11:10:29 +0530

    Reverse the string

---
+++ src/JavaPrograms/PracticeProgram2.java
@@ -0,0 +1,24 @@
+package JavaPrograms;
+
+public class PracticeProgram2 {
+
+	public static void main(String[]  args)
+    {
+         String s="RASHMIKA";
+         
+         String rev=" ";
+         for(int i=s.length()-1;i>=0;i--) 
+         {
+        	 rev=rev+ s.charAt(i);
+         }
+
+		System.out.print(rev);
+
+
+
+
+
+    }
+
+
+}
