a7929093144aa5fa9beb001980724ef49d97209a
Reshma <Reshma@LAPTOP-3PFHDHI7>
2024-06-14 10:50:00 +0530

    Additional changes

--- src/JavaPrograms/PracticeProgram.java
+++ src/JavaPrograms/PracticeProgram.java
@@ -6,7 +6,7 @@ public class PracticeProgram {
     {
     	int a=23;
     	int b=20;
-    	System.out.print(a+b);
+    	System.out.print(a+b+ "this is the addition which i used for calculation of two integers");
     	
     }
 
