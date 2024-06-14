008e1ca34ea1be9698baa428b45a88bd9831b20f
Reshma <Reshma@LAPTOP-3PFHDHI7>
2024-06-14 10:25:22 +0530

    my new branch

---
+++ src/JavaPrograms/PracticeProgram.java
@@ -0,0 +1,22 @@
+package JavaPrograms;
+
+public class PracticeProgram {
+
+    public static void main(String[]  args)
+    {
+    	int a=23;
+    	int b=20;
+    	System.out.print(a+b);
+    	
+    }
+
+
+
+
+
+
+
+
+
+
+}
