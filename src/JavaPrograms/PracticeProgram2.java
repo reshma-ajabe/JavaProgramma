7c87a29fbe2a00277ed9fdb358c9aef4b7ae6ae8
Reshma <Reshma@LAPTOP-3PFHDHI7>
2024-06-15 18:50:30 +0530

    Adding some changes

--- src/JavaPrograms/PracticeProgram2.java
+++ src/JavaPrograms/PracticeProgram2.java
@@ -23,15 +23,15 @@ public class PracticeProgram2 {
 		String a="BhavnacorpaB";
 		char b[]=a.toCharArray();
 		for(int i=0;i<=b.length-1;i++)
-{ 
+        { 
 			int count =1;
 	    for(int j=i+1;j<=b.length-1;j++)
 	    {
-	    	if(b[i]==b[j])
-	    			{
-	    		       count++;
-	    			}
-	    }
+	    if(b[i]==b[j])
+	    {
+	    		 count++;
+	     }
+	   }
 	    System.out.println(b[i]+" frequncy is "+count);
       }
 		
