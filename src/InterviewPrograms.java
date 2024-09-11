ea43fe2d84075d61a03414632c6473cd463f9ff2
Reshma <Reshma@LAPTOP-3PFHDHI7>
2024-09-11 17:57:13 +0530

    Interview PROGRAME

---
+++ src/InterviewPrograms.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class InterviewPrograms {
+	
+	public static void main(String[] args)
+	{
+		String a="reshmaagdhsjaaxx";
+		char b[]=a.toCharArray();
+		Map<Character,Integer>charmap=new HashMap<>();
+		for(char keys:b) {
+			if(charmap.containsKey(keys))
+			{
+				charmap.put(keys,charmap.get(keys)+1);
+			}
+			else
+			{
+				charmap.put(keys,1);
+			}
+		}
+		System.out.println(charmap);
+		
+		
+		
+	}
+	
+	
+	
+
+}
